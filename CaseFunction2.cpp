2f8d1267c4cefb2a0814cdd33f88671d70dff1f7
orestotel <orekpk@gmail.com>
2019-09-16 07:20:00 +0300

    Update CaseFunction2.cpp

--- CaseFunction2.cpp
+++ CaseFunction2.cpp
@@ -66,7 +66,6 @@ void FourthFunc(){
     ... Input \'1\', \'2\' or \'3\'"
     cin>>y;
     if(y==1){
-<<<<<<< HEAD
       x = Cos(x); y="cosine"
     }
     if(y==2){
@@ -76,17 +75,6 @@ void FourthFunc(){
       x =  tan(x);y="tangens"
     }
 cout<<x<<" is the result of "<<y;
-=======
-      x = Cos(x);
-    }
-    if(y==2){
-      x = sin(x);
-    }
-    if(y==3){
-      x =  tan(x);
-    }
-cout<<x<<" result";
->>>>>>> 1ac6c311553879026fa637a3e88acc73fda3a952
   }
 int main(){
 double x;
