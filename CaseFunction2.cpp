d152fcc2db772aa6a48dc99f4f7cbfc97bd8b45b
orestotel <orekpk@gmail.com>
2019-09-16 07:18:52 +0300

    Finish CaseFunction2.cpp

--- CaseFunction2.cpp
+++ CaseFunction2.cpp
@@ -1,17 +1,85 @@
 #include <iostream>
 #include <cmath> // for sine function
 using namespace std;
+void FirstFunc(){
+  double x;//The First Task
+  cout<<"This is the first task\n Input value:";
+  cin>>x;
+  if(x>0){
+    x = 2*sin(x);
+  }
+  if(x<=0){
+    x = 6-x;
+  }
+  cout<<"The result is "<<x<<", compilation over.";
+}
+void SecondFunc(){
+  double x;
+  cout<<"This is the second task\n Input value:";
+  cin>>x;
+  if(x<-2||x>2){
+    x = x*2;
+  }
+  else x=-3*x;
+  cout<<"The result is "<<x<<", compilation over.";
+}
+void ThirdFunc(){
+  double x;
+  cout<<"This is the third task\n Input value:";
+  cin>>x;
+  if(x<=0){
+    x=-x;
+  }
+  if(x<2&&x>0){
+    x=x*x;
+  }
+  if(x>=2){
+    x=4;
+  }
+  cout<<"The result is "<<x<<", compilation over.";
+}
+void FourthFunc(){
+  double x;
+    cout<<"This is the fourth task\n Input value:";
+    cin>>x;
+    if(x<0){
+      x=0;
+    }
+    if((x<=x%2){
+      x=1;
+    }
+    if(x<=x%2+1){
+      x=-1;
+    }
+    cout<<"The result is "<<x<<", compilation over.";
+}
+  void FinalFunc(){
+    double x;
+    char y;
+    cout<<"This is the last tast with TEXT INTERFACE.\n"<<endl;
+    cout<<"Please enter your X value for further operations";
+    cin>>x;
+    cout<<"\n Now, Please choose an option:\n \n
+    1) calculate CoSine\n
+    2) calculate   Sine\n
+    3) calculate tangens\n
+    ... Input \'1\', \'2\' or \'3\'"
+    cin>>y;
+    if(y==1){
+      x = Cos(x); y="cosine"
+    }
+    if(y==2){
+      x = sin(x); y="sine"
+    }
+    if(y==3){
+      x =  tan(x);y="tangens"
+    }
+cout<<x<<" is the result of "<<y;
+  }
 int main(){
 double x;
-cout<<"Input value:";
-cin>>x;
-if(x>0){
-  x = 2*sin(x);
-}
-if(x<=0){
-  x = 6-x;
-}
-cout<<"The result is "<<x<<", compilation over.";
+FirstFunc();SecondFunc();ThirdFunc();FourthFunc();
+FinalFunc();
   system("wait");
   return 0;
 }
