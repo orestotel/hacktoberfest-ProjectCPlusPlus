8902302dd69bfdf71aa5b29eac480206ee434d2c
Your Name <you@example.com>
2019-10-09 23:48:13 +0200

    Fixed issue 7

--- delete_row.cpp
+++ delete_row.cpp
@@ -1,41 +1,44 @@
 //дано прямокутну матрицю м*н. Видалити рядок, сума елементів якого мінімальна
-//This program still doesn't delete the row with the lowest element,
-//neither it does delete the line with the lowest sum, which is the second step
-// #ToDo
+//You forgot to assign count to some value at the start of the program, that's why it returned strange values.
+//The deleting cycle was in some strange for loop that contained also the output cycle, I think that wasn't yout intention so i removed it and replaced by two loops
+//one that removes row with lowest value and one that removes row with lowest sum.
+
 #include <iostream>
 using namespace std;
 
 int main()
 {
-  int m, n,count, max;
+	int row, line, count = 0, max = 0, minEl, minElPos = 0, lowestSum, lowestSumPos;
+	bool writeLowestSum = true;
+
  const int dou = 2;
   {
     cout << "Input row:" << endl;
-    cin >> m;
+    cin >> row;
     cout << "Input Lines" << endl;
-    cin >> n;
+    cin >> line;
   } //set dimensions
   cout<<endl;
-int carr[m][dou];
+int carr[row][dou];
 //set memory
-  int** a = new int* [m];
+  int** a = new int* [row];
 
-  for (int i = 0; i < m; ++i)
+  for (int i = 0; i < row; ++i)
   {
-    a[i] = new int[n];
+    a[i] = new int[line];
   }
 
 //set counting memory
-int b[m];
+int b[row];
 
 //intake cycle
 cout<<"INPUT NUMBERS"<<endl;
-  for (int i = 0; i < m; ++i)
+  for (int i = 0; i < row; ++i)
   {
     cout<<endl;
     cout<<"###########"<<endl;
     cout<<"Row number "<<i<<endl;
-    for (int j = 0; j < n; ++j)
+    for (int j = 0; j < line; ++j)
     {
       cout<<endl;
       cout<<"Element#"<<j<<endl;
@@ -44,66 +47,73 @@ cout<<"INPUT NUMBERS"<<endl;
   }
 //calculation cycle
 max=a[0][0];
+minEl = a[0][0];
+minElPos = 0;
+
 cout<<endl;
 cout<<"End of INPUT"<<endl;
 cout<<"CALCULATING....."<<endl;
 cout<<endl;
-  for (int i = 0; i < m; ++i)
+  for (int i = 0; i < row; ++i)
   {
-    for (int j = 0; j < n; ++j)
+    for (int j = 0; j < line; ++j)
     {
+		if (a[i][j] < minEl)
+		{
+			minEl = a[i][j];
+			minElPos = i;
+		}
 
+		count += a[i][j];
       if(a[i][j]>max){
-        count+=a[i][j];
+		  
         max = a[i][j];
         cout<<max<<"  ["<<i<<";"<<j<<"]"<<endl;
       }
       else{cout<<max<<" Not changed;"<<endl;}
     }
-  //  cout<<"^^"<<count<<"^";//does not work (BUG).
-                              // Must return sum of row
+    cout<<"^^"<<count<<"^" << endl;
+	if (writeLowestSum)
+	{
+		lowestSum = count;
+		lowestSumPos = i;
+		writeLowestSum = false;
+	}
+	if (count < lowestSum)
+	{
+		lowestSum = count;
+		lowestSumPos = i;
+	}
+
     count=0;
   }
   cout<<"successfully calculated."<<endl;
   cout<<endl;
-cout<<"Deleting"<<endl;
-//deleting cycle
-for (int k = 0; k < m; ++k)
-{
-  for (int o = 0; o < n; ++o)
+  cout<<"Deleting"<<endl;
+  //deleting cycle
+  //here "delete" row with lowest element
+  for (int k = 0; k < line; ++k)
   {
-    cout<<a[k][o]<<" (i;j) element"<<endl;
-    if(max==a[k][o]){
-      cout<<a[k][o]<<" Element;"<<endl;
-        for (int j = 0; j < o; ++j)
-        {
-         a[k][j]=0;
-         cout<<a[k][j]<<"debug"<<endl;
-        }
-        cout<<endl;
-
-      cout<<" deleted ("<<k<<" "<<o<<")"<<endl;
-
-    }
-    cout<<a[k][o]<<" result;"<<endl;
+   	a[minElPos][k] = 0;
   }
+
+  for (int k = 0; k < line; ++k)
+  {
+	a[lowestSumPos][k] = 0;
+  }
+
   cout<<"End deleting..."<<endl;
   cout<<endl;
-//output cycle
-cout<<"OUTPUT:"<<endl;
-  for (int i = 0; i < m; ++i)
+  //output cycle
+  cout<<"OUTPUT:"<<endl;
+  for (int i = 0; i < row; ++i)
   {
-    for (int j = 0; j < n; ++j)
+    for (int j = 0; j < line; ++j)
     {
       cout<< a[i][j]<<"'";
     }
     cout<<endl;
   }
   cout<<"Max. Element: "<<max<<endl;
-
-
-
-
   return 0;
-}
 }
