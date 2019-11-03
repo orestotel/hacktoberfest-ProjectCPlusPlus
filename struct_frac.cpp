59edfe8774112654371223e07fb69ac864d51265
orestotel <orekpk@gmail.com>
2019-11-03 19:43:25 +0200

    evens work 0.2

--- struct_frac.cpp
+++ struct_frac.cpp
@@ -4,6 +4,7 @@ using namespace std;
 int i = 0;
 struct frac{
   int up, dw, ev=0;
+  int r_up=0;
   double exact;
 };
 void inp(frac &a){
@@ -11,31 +12,41 @@ void inp(frac &a){
   cout<<"Input fraction:"<<endl;
   cout<<"up: ";
   cin>>a.up;
-  cout<<endl;
   cout<<a.up<<" recorded;"<<endl;
   cout<<"------"<<endl;
   cout<<"dw: ";
   cin>>a.dw;
-  cout<<endl;
   cout<<a.dw<<" recorded;"<<endl;
   cout<<"-----------";
   cout<<endl;
   cout<<endl;
 }
-void add(frac a, frac b){
+frac add(frac a, frac b){
   frac res;
   res.dw = a.dw*b.dw;
   res.up = a.up*b.dw + b.up*a.dw;
-}
+
+    if(res.up>=res.dw){
+      res.r_up = res.up;
+      while(res.r_up>=res.dw){
+        res.r_up= res.r_up-res.dw;
+        res.ev = res.ev+1;
+      }
+    }
+
+  return res;
+};
 void print(frac a){
 cout<<"Fraction: up:"<<a.up;
 cout<<" dw:"<<a.dw;
 cout<<" ev:"<<a.ev;
+a.exact = a.up/a.dw;
 cout<<" exact:"<<a.exact<<endl;
-
+cout<<a.up<<"/"<<a.dw<<"= <"<<a.ev<<" | "<<a.r_up<<"/"<<a.dw<<">"<<endl;
 }
 int main(){
 frac a,b, res;
 inp(a);
-print(a);
+inp(b);
+print(add(a,b));
 }
