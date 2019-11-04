2886107d6c02841af9dcd3b5250590dc8a370eac
orestotel <orekpk@gmail.com>
2019-11-04 15:15:01 +0200

    rename to test2_2.cpp

--- test2_2.cpp
+++ test2_2.cpp
@@ -6,54 +6,69 @@ int main(){
   cout<<"Enter the dimension NxN:";
   cin>>n;
   int actual = 3*n-2;
+  int ac_c = actual;
   cout<<endl;
     int **arr = new int* [n];
     int *vect = new int[actual];
-  for(int i=0; i<n;i++){
-    cout<<"Creating the array;"<<endl;
-    arr[i] = new int[n];
-    cout<<endl;
-  }
-  //record
-  cout<<"enter the array"<<endl;
-  for(int i = 0; i<n; i++){
-    for(int j = 0; j<n;j++){
-      cout<<"enter ["<<i<<";"<<j<<"] element:  ";
-      cin>>arr[i][j];
-      cout<<endl;
+    for(int i=0; i<n;i++){
+      cout<<"Creating the array;"<<endl;
+      arr[i] = new int[n];
     }
+    cout<<endl;
+    for(int i=0; i<actual;i++){
+      cout<<"Assigning vect;"<<endl;
+      vect[i] = 1;
+    }
+  //record
+
+//  cout<<"enter the array"<<endl;
+//  for(int i = 0; i<n; i++){
+//    for(int j = 0; j<n;j++){
+//      cout<<"enter ["<<i<<";"<<j<<"] element:  ";
+//      cin>>arr[i][j];
+//      cout<<endl;
+//    }
+//  }
+int cou = 1;
+for(int i = 0; i<n; i++){
+  for(int j = 0; j<n; j++){
+    arr[i][j] = cou;
+    cou++;
   }
+}
   //print
   for(int i = 0; i<n; i++){
     for(int j = 0; j<n;j++){
-      cout<<" ["<<i<<";"<<j<<"]     :";
-      cout<<arr[i][j]<<endl;
+      cout<<arr[i][j]<<" ' ";
     }
+    cout<<endl;
   }
   //calculate
-  for(int j=n-1; j>0; j--){
-    vect[sum] = arr[n-1][j];
-    sum++;
-  }
-
-  cout<<"sum = "<<sum<<";"<<endl;
-
-  for(int i = n-1; i>0; i--){
-    for(int j = n-1; j>0; j--){
-      cout<<"["<<i<<"]   sum "<<vect[sum]<<endl;
-      vect[sum] = arr[i][j];
-      sum++;
-  }
+  cout<<"calculation"<<endl;
+for(int i=n-1;i>=1;i--){
+vect[actual - ac_c] = arr[i][n-1];
+cout<<" vect - > "<<arr[i][n-1];
+ac_c--;
 }
-for(int j=n-1; j>0; j--){
-  vect[sum] = arr[0][j];
-  sum++;
+cout<<endl;
+cou = n-1;
+for(int i = 0; i<n; i++){
+  //for(int j = 0; j<n;j++){
+  if(n-2>=i){
+    vect[actual-ac_c] = arr[i][cou];
+    cout<<" vect - > "<<vect[actual-ac_c];
+    cou--;
+  }
+  //  }
 }
+cout<<endl;
+for(int i=n-1;i>=0;i--){
+  vect[actual-ac_c] = arr[i][0];
+  cout<<" vect - > "<<vect[actual-ac_c];
+  ac_c--;
+}
+cout<<endl;
 //print RESULT
-cout<<"VECTOR CREATED:"<<endl;
-for(int i=0;i<sum;i++){
-  cout<<" vector: "<< vect[i]<<" ;"<<endl;
-
-}
-
+cout<<"VECTOR CREATED! :)"<<endl;
+return 0;
 }
