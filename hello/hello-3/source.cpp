f7708d40f029a359785d6dfc84d402007b50e53d
orestotel <orekpk@gmail.com>
2019-12-25 17:30:37 +0200

    saved

--- hello/hello-3/source.cpp
+++ hello/hello-3/source.cpp
@@ -5,22 +5,50 @@ using namespace std;
 class shape{
   //figure - число
 protected:
-
+  int a;
 public:
-
+shape(){
+  a = 0;
+}
+virtual double area()=0;
 };
-class rectangle{
+class triangle:public shape{
 protected:
+  int b;
+  int c;
 public:
+triangle():shape(){
+  b=0;
+  c=0;
+}
+double area(){
+  double p=(a+b+c)/2;
+  return sqrt(p*(p-a)*(p-b)*(p-c))
+}
 };
-class square{
+class rectangle:public shape{
+private:
+  int b;
+public:
+  rectangle():shape(){
+    b = 0;
+  }
+  double area(){
+    return a*b;
+  }
+};
+class square:public rectangle{
 private:
 public:
+  square(){
+    a = b = 0;
+  }
+  double area(){
+    return pow(a,2);
+  }
+  
 };
-class triangle{
-private:
-public:
-};
+
 int main(){
 
 }
